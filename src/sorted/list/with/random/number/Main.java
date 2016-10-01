91e7c2b6155bba8f595fa57f9292494d3810c883
leetaejun <leetaejun147@gmail.com>
2016-10-01 18:23:39 +0900

    0~100 사이의 숫자 배열을 O(N)의 시간으로 정렬시키는 문제

--- src/sorted/list/with/random/number/Main.java
+++ src/sorted/list/with/random/number/Main.java
@@ -1,31 +1,43 @@
 package sorted.list.with.random.number;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
+import java.util.stream.Collectors;
 
+
+/**
+ * 0~100의 랜덤한 숫자가 있는 배열이 있을 때, O(N)의 시간내로 정렬된 리스트를 만들기 위한 알고리즘 문제풀이
+ * 
+ * @author leetaejun
+ *
+ */
 public class Main {
 	public static void main(String args[]) {
 		Scanner numberScanner = new Scanner(System.in);
 		
 		List<Integer> numberList = new LinkedList<>();
-		while (true) {
-			System.out.print("리스트에 넣을 숫자를 입력해주세요.");
-			Integer inputNumber = numberScanner.nextInt();
-			
-			if (inputNumber >= 0) {
-				sortListWithInputNumber(numberList, inputNumber);	
-			} else {
-				System.out.println("0이상의 큰 숫자를 입력해주세요.");
-			}
-			
-			
-			for (int index = 0; index < numberList.size(); index++) {
-				System.out.println("index : " + index + ", nubmer : " + numberList.get(index));
+		int[] numberCountArray = new int[100+1];
+		
+		int[] testArray = {19, 21, 10, 1, 100, 99, 54, 56, 88, 12, 11, 13, 13, 0};
+		
+		for (int i = 0; i < testArray.length; i++) {
+			numberCountArray[testArray[i]] += 1;
+		}
+		
+		for (int j = 0; j < numberCountArray.length; j++) {
+			for (int k = numberCountArray[j]-1; k >= 0; k--) {
+				numberList.add(j);
 			}
 		}
+		
+		System.out.println(numberList.stream()
+									 .map(String::valueOf)
+									 .collect(Collectors.joining(", ")));
 	}
 	
+	/*
 	public static void sortListWithInputNumber(List<Integer> numberListToSort, Integer inputNumber) {
 		int getLastIndexByList = numberListToSort.lastIndexOf(inputNumber.intValue());
 		if (getLastIndexByList == -1) {
@@ -78,4 +90,5 @@ public class Main {
 			return midIndex;
 		}
 	}
+	*/
 }
