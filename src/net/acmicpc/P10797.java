ca96768c5da8977a2f5d2fdb4bdb439954ca3c62
leetaejun <leetaejun147@gmail.com>
2016-10-09 19:21:52 +0900

    백준 온라인 저지 문제풀이 - 10797

---
+++ src/net/acmicpc/P10797.java
@@ -0,0 +1,23 @@
+package net.acmicpc;
+
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+public class P10797 {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		
+		int limitedCarNumber = scanner.nextInt();
+		int resultCount = 0;
+		for (int i = 0; i < 5; i ++) {
+			int inputNumber = scanner.nextInt();
+			
+			if (limitedCarNumber == inputNumber) {
+				resultCount++;
+			}
+		}
+		
+		System.out.println(resultCount);
+	}
+}
